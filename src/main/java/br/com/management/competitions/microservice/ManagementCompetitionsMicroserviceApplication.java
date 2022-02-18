c11945d2cf4d1b7c8d951cd241f274e61fd20d35
Rodrigo Prado <digosilva.prado@gmail.com>
2022-02-17 21:58:07 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/management/competitions/microservice/ManagementCompetitionsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.management.competitions.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementCompetitionsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementCompetitionsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/management/competitions/microservice/ManagementCompetitionsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.management.competitions.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementCompetitionsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
